34ad357d75315de04feb5a11e466aef655969efc
Andrea Fumagalli <160780597+fumaghe@users.noreply.github.com>
2024-06-12 17:13:46 +0200

    6.0

--- RICERCA NOAEL 6.0.py
+++ RICERCA NOAEL 6.0.py
@@ -5,31 +5,21 @@ from collections import Counter
 import PyPDF2
 import io
 
-def extract_status_links(url):
+def extract_first_status_link(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'html.parser')
-        tbody = soup.find('tbody')
-        if not tbody:
-            print(f"No tbody found in {url}")
-            return []
 
-        status_links = []
-
-        for row in tbody.find_all('tr')[1:]:
-            link = row.find('a')['href']
-            if "view-attachment" in link:
-                full_status_link = "https://cir-reports.cir-safety.org/" + link.replace("../", "")
-                status_links.append(full_status_link)
-
-        return status_links
+        status_link = soup.find('table').find_all('tr')[1].find('a')['href']
+        full_status_link = "https://cir-reports.cir-safety.org/" + status_link.replace("../", "")
+       
+        return full_status_link
 
     except requests.RequestException as e:
         print(f"Error accessing {url}: {e}")
     except (IndexError, TypeError) as e:
         print(f"Error parsing the page: {e}")
-        return []
 
 def extract_text_from_pdf(pdf_content):
     reader = PyPDF2.PdfReader(io.BytesIO(pdf_content))
@@ -39,40 +29,45 @@ def extract_text_from_pdf(pdf_content):
         text += page.extract_text() or ""
     return text
 
-def find_noael_or_ld50s_values(text):
-    noael_pattern = r'NOAEL\s*[:/]?'
-    ld50s_pattern = r'LD50\s*[:/]?'
-    
-    def find_values(pattern):
-        matches = re.finditer(pattern, text, re.IGNORECASE)
-        values = []
-        for match in matches:
-            start_index = match.end()
-            words = text[start_index:start_index+100].split()[:20]  # Extracting next 20 words
-            for word in words:
-                if re.match(r'\d+(\.\d+)?', word):  # Checking if the word is a number
-                    values.append(word)
-                    break
-        return values
-    
-    noael_values = find_values(noael_pattern)
-    if noael_values:
-        return "NOAEL", noael_values
-    else:
-        ld50s_values = find_values(ld50s_pattern)
-        if ld50s_values:
-            return "LD50s", ld50s_values
-    return None, []
+def find_noael_values(text):
+    pattern = r'NOAEL\s*[:/]?'
+    matches = re.finditer(pattern, text, re.IGNORECASE)
+    values = []
+    for match in matches:
+        start_index = match.end()
+        words = text[start_index:start_index+100].split()[:20]  # Extracting next 20 words
+        for word in words:
+            if re.match(r'\d+(\.\d+)?', word):  # Checking if the word is a number
+                values.append(word)
+                break
+    return values
 
-def find_most_common_value(values):
+def find_most_common_noael(values):
     if not values:
-        return None
+        return None, 0
     count = Counter(values)
-    most_common_count = count.most_common(1)[0][1]
-    most_common_values = [value for value, freq in count.items() if freq == most_common_count]
-    return most_common_values, most_common_count
+    most_common_value, most_common_count = count.most_common(1)[0]
+    return most_common_value, most_common_count
+
+def check_and_append_noael(ingredient_id, most_common_noael, other_noael_values, noael_file_path):
+    with open(noael_file_path, 'a', encoding='utf-8') as file:
+        file.write(f"{ingredient_id}:most_common:{most_common_noael}\n")
+        if other_noael_values:
+            file.write(f"{ingredient_id}:other:{','.join(map(str, other_noael_values))}\n")
 
 def find_ingredient_id_and_extract_link(ingredient_name, data_file_path, noael_file_path):
+    # Load NOAEL values from the NOAEL file into a dictionary
+    noael_dict = {}
+    with open(noael_file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            if ':' in line:
+                id_value_pair = line.strip().split(':')
+                if len(id_value_pair) == 3:
+                    id_key = id_value_pair[0]
+                    if id_key not in noael_dict:
+                        noael_dict[id_key] = {}
+                    noael_dict[id_key][id_value_pair[1]] = id_value_pair[2].split(',')
+
     with open(data_file_path, 'r', encoding='utf-8') as file:
         ingredients = []
         for line in file:
@@ -82,40 +77,44 @@ def find_ingredient_id_and_extract_link(ingredient_name, data_file_path, noael_f
                 search_name in data['pcpc_ciringredientname'].lower()):
                 ingredients.append((data['pcpc_ingredientname'], data['pcpc_ingredientid']))
 
-    with open(noael_file_path, 'a+', encoding='utf-8') as noael_file:
-        noael_file.seek(0)
-        existing_entries = noael_file.readlines()
-        existing_data = {line.split(':')[0]: line.split(':')[1].strip() for line in existing_entries}
-
     if ingredients:
         for ingredient_name, ingredient_id in set(ingredients):
-            if ingredient_id in existing_data:
+            if ingredient_id in noael_dict:
                 print(f"Ingredient name: {ingredient_name}")
-                print(f"Existing Value: {existing_data[ingredient_id]}")
-                continue
-
-            url = f"https://cir-reports.cir-safety.org/cir-ingredient-status-report/?id={ingredient_id}"
-            status_links = extract_status_links(url)
-            for status_link in status_links:
-                try:
-                    response = requests.get(status_link)
-                    response.raise_for_status()
-
-                    pdf_text = extract_text_from_pdf(response.content)
-                    value_type, values = find_noael_or_ld50s_values(pdf_text)
-                    
-                    if values:
-                        most_common_values, most_common_count = find_most_common_value(values)
-                        if most_common_values:
+                most_common_noael = noael_dict[ingredient_id].get('most_common', 'N/A')[0]
+                other_values = noael_dict[ingredient_id].get('other', [])
+                print(f"Most common NOAEL: {most_common_noael} mg/kg")
+                for value in other_values:
+                    print(f"Other NOAEL value: {value} mg/kg")
+            else:
+                url = f"https://cir-reports.cir-safety.org/cir-ingredient-status-report/?id={ingredient_id}"
+                status_link = extract_first_status_link(url)
+                if status_link:
+                    try:
+                        response = requests.get(status_link)
+                        response.raise_for_status()
+                        
+                        pdf_text = extract_text_from_pdf(response.content)
+                        noael_values = find_noael_values(pdf_text)
+                        most_common_noael, _ = find_most_common_noael(noael_values)
+                        
+                        if noael_values:
+                            other_noael_values = set(noael_values)
+                            other_noael_values.discard(most_common_noael)
+                            
                             print(f"Ingredient name: {ingredient_name}")
-                            for value in most_common_values:
-                                print(f"{value_type} value: {value} mg/kg")
-                                noael_file.write(f"{ingredient_id}:{value_type} {value}\n")
-                        break
-                except requests.RequestException as e:
-                    print(f"Error accessing attachment {status_link}: {e}")
-                except Exception as e:
-                    print(f"Error reading PDF {status_link}: {e}")
+                            print(f"Most common NOAEL: {most_common_noael} mg/kg")
+                            for value in other_noael_values:
+                                print(f"Other NOAEL value: {value} mg/kg")
+
+                            check_and_append_noael(ingredient_id, most_common_noael, list(other_noael_values), noael_file_path)
+                        else:
+                            print(f"Ingredient name: {ingredient_name}")
+                            print("No NOAEL values found.")
+                    except requests.RequestException as e:
+                        print(f"Error accessing attachment {status_link}: {e}")
+                    except Exception as e:
+                        print(f"Error reading PDF {status_link}: {e}")
     else:
         print(f"Ingredient containing '{ingredient_name}' not found in the data file.")
 
