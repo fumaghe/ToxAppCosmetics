69fe3f691d7e7a1c6d1f87ba5a4e6c4254af821b
Andrea Fumagalli <160780597+fumaghe@users.noreply.github.com>
2024-07-08 16:54:22 +0200

    OKAY FOTO

--- app/main.py
+++ app/main.py
@@ -1,49 +1,61 @@
 import sys
 import os
+import base64
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'utils')))
 
 import streamlit as st
 from utils.db_utils import load_ingredient_list, find_ingredient_id_and_extract_link, update_search_history
 from utils.findvalue import search_and_update_ingredient
 
-# Configura il layout di Streamlit
+# Imposta il layout di Streamlit
 st.set_page_config(layout="wide")
 
-# CSS per la stilizzazione
+# Percorso all'immagine
+image_path = os.path.join('app', 'static', 'LOGOTOXAPP.png')
+
+# Carica e visualizza l'immagine centrata
+def get_image_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+image_base64 = get_image_base64(image_path)
+
+# CSS per centrare l'immagine
 st.markdown(
-    """
+    f"""
     <style>
-    .center-title {
+    .center-image {{
         display: flex;
         justify-content: center;
         align-items: center;
-        font-size: 50px;
-        font-weight: bold;
-        margin-top: 20px;
-        margin-bottom: 20px;
-    }
-    .center-box {
+        margin-top: 0;
+        margin-bottom: 0;
+    }}
+    .center-image img {{
+        width: 350px;  /* Ridimensiona l'immagine */
+    }}
+    .center-box {{
         display: flex;
         justify-content: center;
         align-items: center;
         width: 50%;
         margin: auto;
         padding: 0 20px; /* Adds spacing to the sides */
-    }
-    .search-box {
+    }}
+    .search-box {{
         text-align: center;
         font-size: 24px;
         margin-top: 20px;
         margin-bottom: 20px;
-    }
-    .search-result {
+    }}
+    .search-result {{
         width: 100%;
         font-size: 28px;
         text-align: center;
         margin-top: 20px;
         margin-bottom: 20px;
-    }
-    .result-links, .result-buttons {
+    }}
+    .result-links, .result-buttons {{
         width: 100%;
         font-size: 18px;
         margin-top: 10px;
@@ -51,14 +63,14 @@ st.markdown(
         display: flex;
         justify-content: space-evenly;
         gap: 120px; /* Adds spacing between buttons */
-    }
-    .result-buttons a {
+    }}
+    .result-buttons a {{
         flex: 1;
         display: flex;
         justify-content: center;
         text-decoration: none; /* Removes the underline from links */
-    }
-    .result-buttons a button {
+    }}
+    .result-buttons a button {{
         width: 100%;
         padding: 10px 80px;
         font-size: 16px;
@@ -68,34 +80,37 @@ st.markdown(
         border: 2px solid #ff0000;
         border-radius: 5px;
         flex: 1;
-    }
-    .result-buttons a button:hover {
+    }}
+    .result-buttons a button:hover {{
         background-color: #ff0000;
         color: white;
-    }
-    .cir-results, .pubchem-results {
+    }}
+    .cir-results, .pubchem-results {{
         width: 100%;
         font-size: 20px;
         margin-top: 10px;
         margin-bottom: 10px;
-    }
-    .update-form {
+    }}
+    .update-form {{
         width: 50%;
         margin: auto;
         font-size: 18px;
         margin-top: 20px;
         margin-bottom: 20px;
-    }
-    .divider {
+    }}
+    .divider {{
         border-top: 2px solid #ccc;
         margin: 20px 0;
-    }
+    }}
     </style>
-    """, unsafe_allow_html=True
+    <div class="center-image">
+        <img src="data:image/png;base64,{image_base64}" alt="Logo">
+    </div>
+    """,
+    unsafe_allow_html=True
 )
 
-# Centrare il titolo
-st.markdown('<div class="center-title">Cosmetic Toxicity</div>', unsafe_allow_html=True)
+
 
 ingredient_list = load_ingredient_list()
 
